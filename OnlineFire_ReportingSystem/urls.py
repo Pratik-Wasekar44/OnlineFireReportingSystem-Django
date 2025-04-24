38e86338a4c8edbdebd529d3e206e4dd76d915f8
Pratik Wasekar <pratikwasekar13@gmail.com>
2025-04-24 18:20:44 +0530

    urls.py fix

--- OnlineFire_ReportingSystem/urls.py
+++ OnlineFire_ReportingSystem/urls.py
@@ -31,3 +31,8 @@ urlpatterns = [
     path('logout/', Logout, name='logout'),
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT
+)
