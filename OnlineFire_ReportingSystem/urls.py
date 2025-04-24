dc3a98a542308e7a12f87ae621f4ba5a8395ce69
Pratik Wasekar <pratikwasekar13@gmail.com>
2025-04-24 19:28:58 +0530

    fix html

--- OnlineFire_ReportingSystem/urls.py
+++ OnlineFire_ReportingSystem/urls.py
@@ -32,10 +32,15 @@ urlpatterns = [
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(
     settings.STATIC_URL,
     document_root=settings.STATIC_ROOT
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    # In production, WhiteNoise handles static files
+    pass
